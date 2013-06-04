3fafcf63e37b31bf909016143f37e90e96b86f7e
Samuel Just <sam.just@inktank.com>
2013-06-03 18:47:23 -0700

    lock the old_pg_state

--- src/osd/OSD.cc
+++ src/osd/OSD.cc
@@ -2078,7 +2078,9 @@ void OSD::handle_pg_peering_evt(
       createmap = get_map(epoch);
     } else {
       assert(old_pg_state);
+      old_pg_state->lock();
       createmap = old_pg_state->get_osdmap();
+      old_pg_state->unlock();
     }
 
     PG::RecoveryCtx rctx = create_context();
@@ -2110,6 +2112,7 @@ void OSD::handle_pg_peering_evt(
     case RES_PARENT: {
       assert(old_pg_state);
       PG::RecoveryCtx rctx = create_context();
+      old_pg_state->lock();
       PG *parent = _create_lock_pg(
 	createmap,
 	resurrected,
@@ -2123,6 +2126,7 @@ void OSD::handle_pg_peering_evt(
 	old_pg_state->past_intervals,
 	*rctx.transaction
 	);
+      old_pg_state->unlock();
       parent->handle_create(&rctx);
       parent->write_if_dirty(*rctx.transaction);
       dispatch_context(rctx, parent, osdmap);
